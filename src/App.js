816a71a6bd83327b67abc93b104f846b5df1fc99
Vladislav <vladislav@MacBook-Air-Vladislav.local>
2020-10-20 08:40:04 +0200

    каркас

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header'
+import Tabs from './components/tabs'
+import New from './components/new'
+import Genres from './components/genres'
+import Footer from './components/footer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Tabs />
+      <New />
+      <Genres />
+      <Footer />
     </div>
   );
 }
